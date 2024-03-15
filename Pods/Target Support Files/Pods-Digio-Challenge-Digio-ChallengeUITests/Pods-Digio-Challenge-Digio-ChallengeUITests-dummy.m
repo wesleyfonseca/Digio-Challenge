6cc3361bf1689f46f5d52a296cfc3058fd4ff5ce
Wesley Fonseca <wesley.dias@terceiro-sky.com.br>
2024-03-15 09:17:25 -0300

    Setting cocoapods and libs

---
+++ Pods/Target Support Files/Pods-Digio-Challenge-Digio-ChallengeUITests/Pods-Digio-Challenge-Digio-ChallengeUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Digio_Challenge_Digio_ChallengeUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Digio_Challenge_Digio_ChallengeUITests
+@end
---
+++ Pods/Target Support Files/Pods-Digio-ChallengeTests/Pods-Digio-ChallengeTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Digio_ChallengeTests : NSObject
+@end
+@implementation PodsDummy_Pods_Digio_ChallengeTests
+@end
